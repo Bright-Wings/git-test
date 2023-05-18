d0bc2b3b546cfeccd4ac97b745f333269fc7f6af
BrightWings <3157190946@qq.com>
2023-05-18 12:45:11 +0800

    my first commit

--- src/main/java/GitDemo.java
+++ src/main/java/GitDemo.java
@@ -1,14 +1,5 @@
 public class GitDemo {
     public static void main(String[] args) {
         System.out.println("git1");
-        System.out.println("git2");
-        System.out.println("git3");
-        System.out.println("git4 hot-fix");
-        System.out.println("master test");
-        System.out.println("hot-fix test");
-        System.out.println("push git");
-        System.out.println("update git!!!!!!!!!!!");
-        System.out.println("!!!!");
-        System.out.println("!!!!22323213");
     }
 }
